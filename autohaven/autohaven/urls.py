332f93463980569c4135273e914a20f858e32e69
BEbrar <eb.atabey@gmail.com>
2024-06-27 17:21:50 +0200

    SignUP Static Page Changes

--- autohaven/autohaven/urls.py
+++ autohaven/autohaven/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/',signup, name='signup'),
 ]
---
+++ autohaven/autohaven/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db() 
+            user.save()
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
