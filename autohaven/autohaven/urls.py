8b1c3525c482195e76fb97487135ff51a1bba414
Robert Richartz <robertrichartz1997@outlook.com>
2024-07-01 13:27:00 +0200

    First commit to feature branch of AUT-57

--- autohaven/autohaven/urls.py
+++ autohaven/autohaven/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('autohaven_app.urls')),
+    path('autohaven/', include('autohaven_app.urls')),
 ]
