8cc539a1d10f8f2836fea7e12270314dd0636ccc
BediiG <r.bedii.gumus@gmail.com>
2024-07-01 13:16:15 +0200

    feature/AUT-41
    Distinct profile pages for sellers and regular users. The code is more readable. Minor fixes

--- autohaven/autohaven_app/views.py
+++ autohaven/autohaven_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .dummy_data import dummy_user_regular, dummy_user_seller, dummy_listings, dummy_orders, dummy_offers
 
 def profile(request):
-    return render(request, 'profile.html')
+    # Pass the dummy user to the template for testing
+    context = {
+        'user': dummy_user_seller,  # Change to dummy_user_seller to test seller version
+        'listings': dummy_listings,
+        'orders': dummy_orders,
+        'offers': dummy_offers,
+    }
+
+    return render(request, 'profile.html', context)
