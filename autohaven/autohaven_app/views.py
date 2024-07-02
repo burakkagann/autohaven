ca879ec3ab72137b3bbe1398c2784bca2365ddc9
BediiG <r.bedii.gumus@gmail.com>
2024-07-02 09:51:01 +0200

    Update User Info
    The backend connected to the My Profile section and user can update info

--- autohaven/autohaven_app/views.py
+++ autohaven/autohaven_app/views.py
@@ -3,7 +3,8 @@ from .forms import SignUpForm
 from .dummy_data import dummy_user_regular, dummy_user_seller, dummy_listings, dummy_orders, dummy_offers
 from django.core.paginator import Paginator
 from .models import Listing, Offer
-
+from .forms import UserUpdateForm 
+from django.contrib.auth.models import User
 
 def root(requst):
     return redirect('home/')
@@ -34,7 +35,24 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form })
 
+
 def profile(request):
+    #User
+    #user = request.user    #This will be changed with the code below when login system is activated. For now it gives AnonymousUser. 
+    user = User.objects.get(username='regularuser') # Change username for testing other users type like : regularuser, seller, superuser!!
+
+    is_regular_user = user.groups.filter(name='RegularUsers').exists()
+    is_seller = user.groups.filter(name='Sellers').exists()
+    is_super_user = user.groups.filter(name='SuperUsers').exists()
+
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the same page after saving. Change for other redirects
+    else:
+        form = UserUpdateForm(instance=user)
+
     # Listings
     listings = Listing.objects.all()
     listingsPaginator = Paginator(listings, 5)  # Show 10 listings per page.
@@ -49,14 +67,14 @@ def profile(request):
     offers_page_number = request.GET.get("orders_page")
     offers_page_obj = offersPaginator.get_page(offers_page_number)
 
-
-    # Pass the dummy user to the template for testing
     context = {
-        'user': dummy_user_seller,  # Change to dummy_user_seller to test seller version
+        'form': form,
+        'user': user,
+        'is_regular_user': is_regular_user,
+        'is_seller': is_seller,
+        'is_super_user': is_super_user,
         'listings_page_obj': listings_page_obj,
         'offers_page_obj': offers_page_obj,
-        'listings': dummy_listings,
-        'orders': dummy_orders,
         'offers': dummy_offers,
     }
 
