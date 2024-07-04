bffd896b8da1b23c0af554cb7d3091bbd49ef809
Juan Pablo Méndez Nogales <jpmendezn@gmail.com>
2024-07-04 19:10:29 +0200

    feat: Adds error message to to failed login page and prepared to work on JS for new listing images

--- autohaven/autohaven_app/static/listings/js/create_update_listing.js
+++ autohaven/autohaven_app/static/listings/js/create_update_listing.js
@@ -2,5 +2,7 @@ function selectImages() {}
 function formSetup() {
   const addImagesBtn = document.getElementById("add-images-btn");
   const imagesContainer = document.getElementById("images-container");
+
+  addImagesBtn.addEventListener("click", selectImages());
 }
 window.addEventListener("load", formSetup);
