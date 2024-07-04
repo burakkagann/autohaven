bc80e5ee85c7d0007c29bbfafa48f546172b6c0f
Robert Richartz <robertrichartz1997@outlook.com>
2024-07-04 19:36:39 +0200

    AUT-58: Added connection of filter connection to information from database

--- autohaven/autohaven_app/views.py
+++ autohaven/autohaven_app/views.py
@@ -30,7 +30,22 @@ def catalog_page(request):
             'listing': item,
             'image': first_image
         })
-    return render(request, 'catalog.html', {'listing': listing_with_images})
+
+    # Get Filter Values
+    brand = models.Listing.objects.values('brand').distinct()
+    model = models.Listing.objects.values('model').distinct()
+    year = models.Listing.objects.values('year').distinct()
+    body_type = models.Listing.objects.values('body_type').distinct()
+    engine_type = models.Listing.objects.values('engine_type').distinct()
+
+
+    return render(request, 'catalog.html', {
+        'listing': listing_with_images,
+        'brand': brand,
+        'model': model,
+        'year': year,
+        'body_type': body_type,
+        'engine_type': engine_type})
 
 
 def signup(request):
