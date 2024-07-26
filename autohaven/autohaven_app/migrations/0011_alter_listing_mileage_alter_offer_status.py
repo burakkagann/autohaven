52504333738585b613e26ed975a504ca53ac42aa
katerina-itopoulos <167315633+katerina-itopoulos@users.noreply.github.com>
2024-07-26 15:41:39 +0200

    fix dependancy

--- autohaven/autohaven_app/migrations/0011_alter_listing_mileage_alter_offer_status.py
+++ autohaven/autohaven_app/migrations/0011_alter_listing_mileage_alter_offer_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('autohaven_app', '0010_create_initial_images'),
+        ('autohaven_app', "0004_alter_listingmodel_body_type"),
     ]
 
     operations = [
